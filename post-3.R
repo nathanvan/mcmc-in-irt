ee544439bca7932f00e3c7e4564f0915a52b6de0
Nathan VanHoudnos <nathanvan@gmail.com>
2013-10-19 15:41:56 -0400

    renamed and fixed setup script

---
+++ post-3.R
@@ -0,0 +1,18 @@
+##
+## http://mcmcinirt.stat.cmu.edu/setup/post-3.R
+##
+## This is the code from Post 2, necessary to setup Post 3
+##
+## Last update: 19-Oct-2013 by Nathan VanHoudnos
+
+source.setup <- function( code.to.run ) {
+  source(paste("http://mcmcinirt.stat.cmu.edu/setup/",code.to.run,sep=""))
+}
+
+source.setup( "post-2-generate-2pl-data.R" )
+source.setup( "post-2-equate-functions.R" )
+
+## Clean up
+rm(source.setup)
+
+cat("Done.\n")
