ef319db8911d65bed2131abf3071fd05e7524583
Nathan VanHoudnos <nathanvan@gmail.com>
2013-10-22 17:53:55 -0400

    playing with spacing

---
+++ post-3-mh-gibbs-shell-run-A-setup.R
@@ -0,0 +1,6 @@
+## Define the hyper parameters to value from Post 1
+hyperpars <- list( mu.a         = 1.185,
+                   s2.a         = 1.185,
+                   s2.b         = 100,
+                   alpha.th     = 1,
+                   beta.th      = 1 )
--- post-4-run-B-viz.R
+++ post-4-run-B-viz.R
@@ -1,3 +1,4 @@
 ## N.B. ylim specifies the scale on the y-axis. 
-plot( run.B.mcmc[, get.2pl.params(1:9,NULL,NULL,NULL)], density=FALSE,
+plot( run.B.mcmc[, get.2pl.params(1:9,NULL,NULL,NULL)],
+      density=FALSE,
       ylim=c(-5,5), smooth=TRUE )
