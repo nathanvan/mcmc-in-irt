940b0e1aec852927e1ecd05c838ef69a8f2bc2e8
Nathan VanHoudnos <nathanvan@gmail.com>
2013-10-24 21:18:49 -0400

    added smoothers to post 7 extra plotting

--- post-7-plot-extra.R
+++ post-7-plot-extra.R
@@ -1,11 +1,11 @@
 ## 9 person ability parameters
 plot( run.C.mcmc[, get.2pl.params(1:9,NULL,NULL,NULL)], 
-      density=FALSE, ylim=c(-5,5) )
+      density=FALSE, smooth=TRUE, ylim=c(-5,5) )
 
 ## 9 item discrimination parameters
 plot( run.C.mcmc[, get.2pl.params(NULL,1:9,NULL,NULL)], 
-      density=FALSE, ylim=c(-2,2) )
+      density=FALSE, smooth=TRUE, ylim=c(-2,2) )
 
 ## 9 item difficulty parameters
 plot( run.C.mcmc[, get.2pl.params(NULL,NULL,1:9,NULL)], 
-      density=FALSE, ylim=c(-4,-1) )
+      density=FALSE, smooth=TRUE, ylim=c(-4,-1) )
