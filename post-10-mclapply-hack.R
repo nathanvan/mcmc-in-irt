85e0e403f5d2a69107feb214fbbdcb46ff5f6721
Nathan VanHoudnos <nathanvan@gmail.com>
2014-08-26 15:08:43 -0500

    Updated mclapply.hack

--- post-10-mclapply-hack.R
+++ post-10-mclapply-hack.R
@@ -1,11 +1,24 @@
-## Define a hackish version of mclapply for Windows
+##
+## post-10-mclapply.hack.R
+##
+## Nathan VanHoudnos
+## nathanvan AT northwestern FULL STOP edu
+## July 14, 2014
+##
+## A script to implement a hackish version of 
+## parallel:mclapply() on Windows machines.
+## On Linux or Mac, the script has no effect
+## beyond loading the parallel library. 
+
+require(parallel)    
+
+## Define the hack
 mclapply.hack <- function(...) {
     ## Create a cluster
-    ## ... How many workers do you need?
     size.of.list <- length(list(...)[[1]])
     cl <- makeCluster( min(size.of.list, detectCores()) )
 
-    ## Find out the loaded packages 
+    ## Find out the names of the loaded packages 
     loaded.package.names <- c(
         ## Base packages
         sessionInfo()$basePkgs,
@@ -28,6 +41,7 @@ mclapply.hack <- function(...) {
                      envir=globalenv())
        
        ## Load the libraries on all the clusters
+       ## N.B. length(cl) returns the number of clusters
        parLapply( cl, 1:length(cl), function(xx){
            lapply(loaded.package.names, function(yy) {
                require(yy , character.only=TRUE)})
@@ -53,7 +67,7 @@ if( Sys.info()[['sysname']] == 'Windows' ){
       "   \n\n",
       "   As a quick hack, we replace this serial version of mclapply()\n",
       "   with a wrapper to parLapply() for this R session. Please see\n\n",
-      "      http://mcmcinirt.stat.cmu.edu \n\n",
+      "     http://www.stat.cmu.edu/~nmv/2014/07/14/implementing-mclapply-on-windows \n\n",
       "   for details.\n\n"))
 }
 
@@ -64,3 +78,5 @@ mclapply <- switch( Sys.info()[['sysname']],
    Windows = {mclapply.hack}, 
    Linux   = {mclapply},
    Darwin  = {mclapply})
+
+## end post-10-mclapply.hack.R
