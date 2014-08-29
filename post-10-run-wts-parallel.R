44dac146e3778326c3ee5089feacae0a206b5fd2
Nathan VanHoudnos <nathanvan@gmail.com>
2014-08-29 12:08:11 -0500

    fixed mcmcinirt typo

--- post-10-run-wts-parallel.R
+++ post-10-run-wts-parallel.R
@@ -3,11 +3,12 @@
 ##   (ii) if the current R session is running on a Windows
 ##        machine, it implements a custom version of
 ##        parallel:mclapply(). 
-source("http://mcmcinrt.stat.cmu.edu/setup/post-10-mclapply-hack.R")
+source("http://mcmcinirt.stat.cmu.edu/setup/post-10-mclapply-hack.R")
 
 ## Note two changes to the wait.then.square call: 
 ##   (i)   lapply becomes mclapply, and 
-##   (ii)  mc.cores (the number of processors to use in parallel) 
+##   (ii)  mc.cores (the number of processors to use in parallel)
+##         is set to 4. 
 system.time( par.output <- mclapply( 1:4, wait.then.square,
                                      mc.cores=4             ) )
 ##  user  system elapsed 
